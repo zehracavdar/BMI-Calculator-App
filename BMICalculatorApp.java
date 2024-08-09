e0de15cf8e6619dac7461a8be59c268d1f29b9db
Zehra Çavdar <110056971+zehracavdar@users.noreply.github.com>
2024-08-09 11:25:52 +0300

    BMICalculatorApp.java updated

--- BMICalculatorApp.java
+++ BMICalculatorApp.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-@SuppressWarnings("Convert2Lambda")
+
 public class BMICalculatorApp {
     public static void main(String [] args) {
         JFrame frame = new JFrame ("BMI Calculator");
@@ -22,24 +22,25 @@ public class BMICalculatorApp {
 
         calculateButton.addActionListener(new ActionListener() {
 
-                                              @Override
-                                              public void actionPerformed(ActionEvent e) {
-                                                  // TODO Auto-generated method stub
-                                                  try {
-                                                      double weight = Double.parseDouble(weightText.getText().trim());
-                                                      double height = Double.parseDouble(heightText.getText().trim());
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+            try {
+            double weight = Double.parseDouble(weightText.getText().trim());
+            double height = Double.parseDouble(heightText.getText().trim());
 
-                                                      BMICalculator calculator = new BMICalculator();
-                                                      double bmi = calculator.calculate(height, weight);
-                                                      String category = calculator.BMItoCategory(bmi);
+            BMICalculator calculator = new BMICalculator();
+            double bmi = calculator.calculate(height, weight);
+            String category = calculator.BMItoCategory(bmi);
 
-                                                      resultLabel.setText(String.format("%.1f - %s", bmi, category));
-                                                  }
-                                                  catch (NumberFormatException e1) {
-                                                      resultLabel.setText("Invalid input");
-                                                  }
-                                              }
-                                          }
+            resultLabel.setText(String.format("%.1f - %s", bmi, category));
+            }
+                catch (NumberFormatException e1) {
+                resultLabel.setText("Invalid input");
+                    
+                        }
+                  }
+            }
         );
 
 
